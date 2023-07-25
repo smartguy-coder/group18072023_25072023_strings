eca5c37bb19b1169ff578d5fff056d8ab2d25ea5
Василь Картичак <w.i.k.mailua@gmail.com>
2023-07-25 20:02:27 +0300

    intro strings

---
+++ strings.py
@@ -0,0 +1,45 @@
+# my_cat = 'My cat'
+#
+# my_greeting = "I'm David"
+# my_greeting2 = 'I\'m \nDavid and \tI\'m a man!!!'
+#
+# eneida_chunk = """
+# Еней був парубок моторний \U000026F7
+# І хлопець хоть куди козак 😂,
+# Удавсь на всеє зле проворний,
+# Завзятійший од всіх бурлак.
+# Но греки, як спаливши Трою,
+# Зробили з неї скирту гною,
+# Він взявши торбу тягу дав;
+# Забравши деяких троянців,
+# Осмалених, як гиря, ланців,
+# П'ятами з Трої накивав.
+# """
+# print(eneida_chunk)
+# #
+# # myltiplicated = my_cat * 15
+# # print(myltiplicated)
+# # print('*' * 150)
+# #
+# # summa = my_greeting + my_cat
+# # print(summa)
+#
+# skier_unicode = '\U000026F7'
+# print(skier_unicode)
+#
+# print('\U00000490')
+#
+# roman_4 = '\U00002163'
+# print(roman_4)
+
+intro = 'My'
+name = 'name'
+string3 = 'is'
+string4 = 'really'
+string5 = 'long'
+
+sentence = intro + ' ' + name + ' ' + string3 + ' ' + string4 + ' ' + string5 + '.'
+print(sentence)
+
+good_formed_sentence = f'{intro} {name} {string3} {string4} {string5}.'
+print(good_formed_sentence)
